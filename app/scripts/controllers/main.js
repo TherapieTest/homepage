3cf9bb558f6be5a43f8ff9210af7cb5508976e40
Michael Weichselbaumer <michael.weichselbaumer@ntsretail.com>
2015-11-12 19:50:20 +0100

    add google-map

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -14,4 +14,17 @@ angular.module('therapiepapstApp')
       'AngularJS',
       'Karma'
     ];
+
+    this.map = { 
+    	center: { 
+    		latitude: 48.315545, 
+    		longitude: 14.280145 
+    	},
+		options: {
+			mapTypeId: google.maps.MapTypeId.SATELLITE,
+			mapTypeControl: true
+		},
+    	zoom: 18
+    };
+
   });
