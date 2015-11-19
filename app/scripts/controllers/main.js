c17c41e47c85fe2f323b6ec3a55b206adac33ebd
Therapie Papst <tech@therapie-papst.at>
2015-11-19 21:19:34 +0100

    ahhh

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -51,5 +51,19 @@ angular.module('therapiepapstApp')
       },
       zoom: 18,
     };
+  }.directive('scrollTo', function ($location, $anchorScroll) {
+      return function (scope, element, attrs) {
 
-  });
+        element.bind('click', function (event) {
+          event.stopPropagation();
+          var off = scope.$on('$locationChangeStart', function (ev) {
+            off();
+            ev.preventDefault();
+          });
+          var location = attrs.scrollTo;
+          $location.hash(location);
+          $anchorScroll();
+        });
+
+      };
+    }));
