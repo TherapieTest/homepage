768da9c0e4249bfa3ab08a8b44f9f58ef283080a
Therapie Papst <tech@therapie-papst.at>
2015-11-27 10:10:03 +0100

    make filerev work for css files

--- Gruntfile.js
+++ Gruntfile.js
@@ -265,7 +265,9 @@ module.exports = function (grunt) {
           '<%= yeoman.dist %>/styles'
         ],
         patterns: {
-          js: [[/(images\/[^''""]*\.(png|jpg|jpeg|gif|webp|svg))/g, 'Replacing references to images']]
+          js: [[/(images\/[^''""]*\.(png|jpg|jpeg|gif|webp|svg))/g, 'Replacing references to images']],
+          css: [[/(images\/.*?\.(?:png|jpg|jpeg|gif|webp|svg))/g, 'Replacing references to images']],
+          html: [[/(images\/[^''""]*\.(png|jpg|jpeg|gif|webp|svg))/g, 'Replacing references to images']]
         }
       }
     },
