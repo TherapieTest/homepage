1c591c22adf3762b72d8412da1af65fa7f69a825
TherapieTest <test@therapie-papst.at>
2018-05-06 12:35:07 +0200

    update travis configuration

--- sync-ghpages.sh
+++ sync-ghpages.sh
@@ -7,7 +7,7 @@ git init
 
 # inside this git repo we'll pretend to be a new user
 git config user.name "Travis CI"
-git config user.email "tech@therapie-papst.at"
+git config user.email "test@therapie-papst.at"
 
 # The first and only commit to this new Git repo contains all the
 # files present with the commit message "Deploy to GitHub Pages".
